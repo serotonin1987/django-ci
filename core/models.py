8efd5802b557ed5fe9028e1e4ed49fbfc649b042
Шугаев Халиль Асылбекович <ShugaevKha@atyrau.itstep>
2026-09-09 09:50:33 +0500

    New testн

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from os import name
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(validators=[MinValueValidator(0)])
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from core.models import Product
 
 
-class HomeTest(TestCase):
-    def test_one_plus_one(self):
-        self.assertEqual(1 + 1, 2)
+class ProductTestCase(TestCase):
+
+    def check_product(self, product):
+        self.assertEqual(product.name, "iPhone")
+        self.assertEqual(product.price, 500000)
+        self.assertEqual(product.quantity, 10)
+
+    def test_product_creation(self):
+        product = Product.objects.create(name="iPhone", price=500000, quantity=10)
+        self.check_product(product)
+
+    def test_negative_price(self):
+        product = Product(name="iPhone", price=-100, quantity=10)
+        with self.assertRaises(ValidationError):
+            product.full_clean()
