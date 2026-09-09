c2b53020f17f1ce7587ea0983ac63acc003c42e3
Шугаев Халиль Асылбекович <ShugaevKha@atyrau.itstep>
2026-09-09 10:22:50 +0500

    Add 0004 migration to restore product quantity column

--- core/models.py
+++ core/models.py
@@ -5,6 +5,6 @@ from django.core.validators import MinValueValidator
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField(validators=[MinValueValidator(0)])
-
+    quantity = models.IntegerField(default=0)
     def __str__(self):
         return self.name
